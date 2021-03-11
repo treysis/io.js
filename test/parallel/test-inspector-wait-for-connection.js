dbad235cedc48aa82e23e9f81e14464159e33cc0
treysis <treysis@gmx.net>
2021-03-11 09:34:24 +0100

    Update test-inspector-wait-for-connection.js

--- test/parallel/test-inspector-wait-for-connection.js
+++ test/parallel/test-inspector-wait-for-connection.js
@@ -7,7 +7,7 @@ const assert = require('assert');
 const { NodeInstance } = require('../common/inspector-helper.js');
 
 async function runTests() {
-  const child = new NodeInstance(['--incpsect=localhost',
+  const child = new NodeInstance(['--inspect=localhost',
                                   '-e', `(${main.toString()})()`], '', '');
   const session = await child.connectInspectorSession();
   await session.send({ method: 'Runtime.enable' });
