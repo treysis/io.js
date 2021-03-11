690be47e1d269bcb7c4b16e95e6abc2c133c68e4
treysis <treysis@gmx.net>
2021-03-11 18:50:55 +0100

    Apply suggestions from code review

--- test/parallel/test-https-client-get-url.js
+++ test/parallel/test-https-client-get-url.js
@@ -46,7 +46,9 @@ const server = https.createServer(options, common.mustCall((req, res) => {
 }, 3));
 
 server.listen(0, common.mustCall(() => {
-  const u = `https://${common.localhostIP}:${server.address().port}/foo?bar`;
+  const u = (common.hasIPv6 ?
+             `https://[${common.localhostIP}]:${server.address().port}/foo?bar` :
+             `https://${common.localhostIP}:${server.address().port}/foo?bar`);
   https.get(u, common.mustCall(() => {
     https.get(url.parse(u), common.mustCall(() => {
       https.get(new URL(u), common.mustCall(() => {
